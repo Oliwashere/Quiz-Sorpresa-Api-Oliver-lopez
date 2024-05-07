f8fd6e7fc68215b2154bd0a102927fb5958db99a
Oli <oliverlopez42069@gmail.com>
2024-05-07 09:31:05 -0500

    feat: :hammer: Final update JS

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ fetch(request).then((Response)  => {
     let e = document.getElementById("phone");
     e.innerHTML = JSON.stringify(data.results[0].phone);
     let f = document.getElementById("password");
-    f.innerHTML = JSON.stringify(data.results[0].login.password);   
+    f.innerHTML = JSON.stringify(data.results[0].login.password);
     let g = document.getElementById("pfp");
     g.innerHTML = JSON.stringify(data.results[0].picture.medium);
 })  
